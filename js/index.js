d7b23967bfea353e1d9875fe219e670375109979
Jacob_RT <jakerobsontull@gmail.com>
2021-02-07 01:44:47 +0000

    Barebones HTML

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function nextDetails(){
+	document.getElementById("register-intial").style.display = "none";
+	document.getElementById("register-further").style.display = "block";
+	//$("#register-intial").hide();
+}
