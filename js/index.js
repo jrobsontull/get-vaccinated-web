8f0c23355880299aedf2f309f534325d880894fa
Jacob_RT <jakerobsontull@gmail.com>
2021-02-07 14:59:41 +0000

    Tidying up front end UI

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 function nextRegisterDetails() {
-	document.getElementById("register-intial").style.display = "none";
-	document.getElementById("register-further").style.display = "block";
+	//document.getElementById("register-intial").style.display = "none";
+	//document.getElementById("register-further").style.display = "block";
+	$('#register-intial').fadeOut(function() {
+		$('#register-further').fadeIn();
+	});
 }
 
 function loginPage() {
-	document.getElementById("registration").style.display = "none";
-	document.getElementById("login").style.display = "block";
+	//document.getElementById("register-div").style.display = "none";
+	//document.getElementById("login").style.display = "block";
+	$('#register-div').fadeOut(function() {
+		$('#login').fadeIn();
+	});
 }
 
 function backToRegister() {
-	document.getElementById("registration").style.display = "block";
-	document.getElementById("login").style.display = "none";
+	$('#register-further').hide();
+	$('#login').fadeOut(function() {
+		$('#register-div').fadeIn();
+		$('#register-intial').fadeIn();
+	});
 }
