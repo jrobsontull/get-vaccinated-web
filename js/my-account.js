c3aa4ebd5861c04049dc581504391f4063c4081d
Jacob_RT <jakerobsontull@gmail.com>
2021-02-18 22:49:19 +0000

    Fix js nav

--- js/my-account.js
+++ js/my-account.js
@@ -1,16 +1,27 @@
-function navigate(nav_to_bar, to_nav) {
+function navigate(nav_to_bar) {
 	to_nav_to_bar = $(nav_to_bar);
+	// Color nav bar
+	to_nav_to_bar.parent().children().each(function () {
+		if ($(this).hasClass('selected')) {
+			$(this).removeClass('selected');
+		}
+	});
+	to_nav_to_bar.addClass('selected');
 
+	// Navigate
 	current_display_blk = $(".options-display").children().each(function () {
 		if ($(this).css('display') == "block") {
 			return $(this);
 		}
 	});
 	
-	// Naviagte
 	current_display_blk.fadeOut(function() {
-		to_nav_to.fadeIn();
-		current_display_blk.removeClass('selected');
-		to_nav_to.addClass('selected');
+		if (to_nav_to_bar.attr('id') == 'personal') {
+			$('.personal').fadeIn();
+		} else if (to_nav_to_bar.attr('id') == 'vaccine-centre') {
+			$('.vaccine-centre').fadeIn();
+		} else if (to_nav_to_bar.attr('id') == 'vaccine-slot') {
+			$('.vaccine-slot').fadeIn();
+		}
 	});
 }
